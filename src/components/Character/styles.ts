b6238ecff3c64e7e03cd9cf633f692a16c4031ac
Somz <matheus_cf_8d@hotmail.com>
2022-06-01 07:23:16 -0300

    first commit

---
+++ src/components/Character/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div<{left: number, top: number, size: number, sidePos: number}>`
+    width: ${props => props.size}px;
+    height: ${props => props.size}px;
+    position: absolute;
+    left: ${props => props.left}px;
+    top: ${props => props.top}px;
+    background-image: url('/assets/char.png');
+    background-position: 0px ${props => props.sidePos}px;
+
+`
